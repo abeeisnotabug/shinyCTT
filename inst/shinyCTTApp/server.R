84e2103d05544908fc9decfd71fd8636d968a4a8
R. Maximilian Bee <r.maximilian.bee@fu-berlin.de>
2018-08-25 17:49:58 +0200

    Got shinyapp working!

--- inst/shinyCTTApp/server.R
+++ inst/shinyCTTApp/server.R
@@ -1,17 +1,52 @@
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-        # Test git!
-
+    output$objectsInWorkspace <- renderUI({
+        selectInput("objectFromWorkspace",
+                    "Choose data object from Workspace:",
+                    Filter(function(object) !is.null(dim(get(object))), ls(envir = globalenv())))
     })
 
-})
+    userData <- reactive({
+        if (input$source == "CSV") {
+            req(input$CSVFile)
+
+            read.csv(input$CSVFile$datapath,
+                     header = input$header,
+                     sep = input$sep,
+                     quote = input$quote)
+        } else if (input$source == "Workspace") {
+            req(input$objectFromWorkspace)
+
+            get(input$objectFromWorkspace)
+        }
+    })
+
+    itemCols <- reactive({
+        eval(parse(text = input$itemColsString))
+    })
+
+    output$dataOverview <- renderTable({
+
+        # Limit max number of columns to display --------
+        #nColsToDisplay <- min(ncol(userData), 9)
+        #return(head(userData[, 1:nColsToDisplay]))
+
+        userDataToDisplay <- userData()
+
+        if (is.null(colnames(userData()))) {
+            colnames(userDataToDisplay) <- sprintf("(%i)", 1:ncol(userData()))
+        } else {
+            colnames(userDataToDisplay) <- sprintf("%s (%s)", colnames(userData()), 1:ncol(userData()))
+        }
+
+        return(head(userDataToDisplay))
+    })
+
+    observeEvent(input$descrStats, {
+        output$descrTable <- renderTable({
+            input$descrStats
+            head(userData()[, isolate({itemCols()})])
+        })
+    })
+}
--- inst/shinyCTTApp/ui.R
+++ inst/shinyCTTApp/ui.R
@@ -1,22 +1,54 @@
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("shinyCTTApp"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+    fluidRow(
+
+        column(3,
+
+            wellPanel(
+                selectInput("source", "Choose source of data:",
+                            choices = c("Workspace", "CSV", "SPSS")),
+                conditionalPanel(condition = "input.source == 'Workspace'",
+                                 uiOutput("objectsInWorkspace")),
+                conditionalPanel(condition = "input.source == 'CSV'",
+                                 fileInput("CSVFile", "Choose CSV File",
+                                           multiple = TRUE,
+                                           accept = c("text/csv",
+                                                      "text/comma-separated-values,text/plain",
+                                                      ".csv")),
+                                 # Input: Checkbox if file has header ----
+                                 checkboxInput("header", "Header", TRUE),
+                                 radioButtons("sep", "Separator",
+                                              choices = c(Comma = ",",
+                                                          Semicolon = ";",
+                                                          Tab = "\t"),
+                                              selected = ","),
+                                 radioButtons("quote", "Quote",
+                                              choices = c(None = "",
+                                                          "Double Quote" = '"',
+                                                          "Single Quote" = "'"),
+                                              selected = '"'))
+                #conditionalPanel(condition = "input.source == 'SPSS'"),
+            ),
+
+            wellPanel(
+                textInput("itemColsString", "Specify item columns:", value = "2:7"),
+                div("Type in item names in double quotes OR the column numbers without quotes
+                    (they are given next to the item names in the table in brackets).
+                    Separate by commas, don't mix quoted item names and column numbers."),
+                hr(),
+                textInput("groupColsString", "Specify group column:", value = "1")
+            ),
+
+            wellPanel(
+                actionButton("descrStats", "Calculate descriptive statistics!")
+            )
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+        column(9,
+               h3("Overview of the data:"),
+               tableOutput("dataOverview"),
+               tableOutput("descrTable"))
     )
-))
+)
