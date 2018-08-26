0583a7b469e32d7793631ed97a6a3ec3dfac7ba8
R. Maximilian Bee <r.maximilian.bee@fu-berlin.de>
2018-08-26 16:10:16 +0200

    Redesign of shiny app, added corrplot!

--- inst/shinyCTTApp/server.R
+++ inst/shinyCTTApp/server.R
@@ -1,5 +1,7 @@
 # Define server logic required to draw a histogram
-function(input, output) {
+function(input, output, session) {
+
+    # Data input -------------------------------------------------------------------------------
 
     output$objectsInWorkspace <- renderUI({
         selectInput("objectFromWorkspace",
@@ -11,41 +13,97 @@ function(input, output) {
         if (input$source == "CSV") {
             req(input$CSVFile)
 
-            read.csv(input$CSVFile$datapath,
-                     header = input$header,
-                     sep = input$sep,
-                     quote = input$quote)
+            userDataTmp <- read.csv(input$CSVFile$datapath,
+                                    header = input$header,
+                                    sep = input$sep,
+                                    quote = input$quote)
         } else if (input$source == "Workspace") {
             req(input$objectFromWorkspace)
 
-            get(input$objectFromWorkspace)
+            userDataTmp <- get(input$objectFromWorkspace)
         }
+
+        if (is.null(colnames(userDataTmp))) colnames(userDataTmp) <- paste("var", 1:ncol(userDataTmp), sep = "_")
+
+        userDataTmp
     })
 
-    itemCols <- reactive({
-        eval(parse(text = input$itemColsString))
-    })
-
-    numericCols <- reactive({
-        colnames(userData())[sapply(userData(), is.numeric)]
-    })
-
-    output$dataOverview <- renderTable({
-
-        # Limit max number of columns to display --------
-        #nColsToDisplay <- min(ncol(userData), 9)
-        #return(head(userData[, 1:nColsToDisplay]))
-
-        userDataToDisplay <- userData()
-
-        if (is.null(colnames(userData()))) {
-            colnames(userDataToDisplay) <- sprintf("(%i)", 1:ncol(userData()))
+    possibleItemColumns <- reactive({
+        if (mode(userData()) == "list") {
+            colnames(userData())[sapply(userData(), is.numeric)]
         } else {
-            colnames(userDataToDisplay) <- sprintf("%s (%s)", colnames(userData()), 1:ncol(userData()))
+            colnames(userData())
         }
-
-        return(head(userDataToDisplay))
     })
+
+    output$dataOverview <- renderDataTable({as.data.frame(userData())})
+
+    output$itemColsChooser <- renderUI({
+        checkboxGroupInput("itemCols",
+                           "Select which columns contain items",
+                           choices = possibleItemColumns(),
+                           selected = possibleItemColumns())
+    })
+
+    output$groupColChooser <- renderUI({
+        selectInput("groupCol",
+                    "Select which column contains the group",
+                    choices = c("Data does not contain group" = "random_group",
+                                colnames(userData())))
+    })
+
+    observeEvent(input$goDescrStats, {
+        appendTab(inputId = "navbar",
+            tabPanel("Descriptive Statistics",
+                     value = "panelDescrStats",
+                sidebarLayout(
+                     sidebarPanel(
+                     ),
+                     mainPanel(
+                         h3("Mean, standard deviation, skewness, excess:"),
+                         #uiOutput("descrWarning"),
+                         tableOutput("descrTable"),
+                         h3("Correlation Plot:"),
+                         plotOutput("corrPlot")
+                     )
+                )
+            ),
+            select = TRUE
+        )
+
+        removeTab(inputId = "navbar",
+                  target = "panelDataInput")
+
+        output$descrTable <- renderTable({
+            isolate({
+                t(apply(userData()[, input$itemCols], 2, function(col) c(Mean = mean(col),
+                                                                     Sd = sd(col),
+                                                                     Skew = moments::skewness(col),
+                                                                     Excess = moments::kurtosis(col) - 3)))
+            })
+        }, rownames = TRUE)
+
+        # Warning message to be implemented later!! ------------------
+
+        #output$descrWarning <-   renderUI({
+        #    val <- correlation()
+        #    if(is.null(val)) {
+        #        tags$i("Waiting for data input...")
+        #    } else {
+        #        isNA <- is.na(val)
+        #        if(sum(isNA)) {
+        #            tags$div(
+        #                tags$h4("Warning: The following pairs in calculated correlation have been converted to zero because they produced NAs!"),
+        #                helpText("Consider using an approriate NA Action to exclude missing data"),
+        #                renderTable(expand.grid(attr(val, "dimnames"))[isNA,]))
+        #        }
+        #    }
+        #})
+
+        output$corrPlot <- renderPlot({corrplot::corrplot.mixed(cor(userData()[, input$itemCols]))})
+    })
+
+    # Descr. statistics -------------------------------------------------------------------------------
 
     observeEvent(input$descrStats, {
         output$caption1 <- renderText({"Mean, standard deviation, skewness, excess, covariance matrix:"})
--- inst/shinyCTTApp/ui.R
+++ inst/shinyCTTApp/ui.R
@@ -1,55 +1,48 @@
 fluidPage(
 
-    # Application title
-    titlePanel("shinyCTTApp"),
+    navbarPage("shinyCTT",
+               id = "navbar",
 
-    fluidRow(
+        # Data input -------------------------------------------------------------------------------
 
-        column(3,
-
-            wellPanel(
-                selectInput("source", "Choose source of data:",
-                            choices = c("Workspace", "CSV", "SPSS")),
-                conditionalPanel(condition = "input.source == 'Workspace'",
-                                 uiOutput("objectsInWorkspace")),
-                conditionalPanel(condition = "input.source == 'CSV'",
-                                 fileInput("CSVFile", "Choose CSV File",
-                                           multiple = TRUE,
-                                           accept = c("text/csv",
-                                                      "text/comma-separated-values,text/plain",
-                                                      ".csv")),
-                                 # Input: Checkbox if file has header ----
-                                 checkboxInput("header", "Header", TRUE),
-                                 radioButtons("sep", "Separator",
-                                              choices = c(Comma = ",",
-                                                          Semicolon = ";",
-                                                          Tab = "\t"),
-                                              selected = ","),
-                                 radioButtons("quote", "Quote",
-                                              choices = c(None = "",
-                                                          "Double Quote" = '"',
-                                                          "Single Quote" = "'"),
-                                              selected = '"'))
-                #conditionalPanel(condition = "input.source == 'SPSS'"),
-            ),
-
-            wellPanel(
-                textInput("itemColsString", "Specify item columns:", value = "2:7"),
-                div("Type in item names in double quotes OR the column numbers without quotes
-                    (they are given next to the item names in the table in brackets).
-                    Separate by commas, don't mix quoted item names and column numbers."),
-                hr(),
-                textInput("groupColsString", "Specify group column:", value = "1")
-            ),
-
-            wellPanel(
-                actionButton("descrStats", "Calculate descriptive statistics!")
+        tabPanel("Data input",
+                 value = "panelDataInput",
+            sidebarLayout(
+                sidebarPanel(
+                    selectInput("source", "Choose source of data:",
+                                choices = c("Workspace", "CSV", "SPSS")),
+                    conditionalPanel(condition = "input.source == 'Workspace'",
+                                     uiOutput("objectsInWorkspace")),
+                    conditionalPanel(condition = "input.source == 'CSV'",
+                                     fileInput("CSVFile", "Choose CSV File",
+                                               multiple = TRUE,
+                                               accept = c("text/csv",
+                                                          "text/comma-separated-values,text/plain",
+                                                          ".csv")),
+                                     checkboxInput("header", "Header", TRUE),
+                                     radioButtons("sep", "Separator",
+                                                  choices = c(Comma = ",",
+                                                              Semicolon = ";",
+                                                              Tab = "\t"),
+                                                  selected = ","),
+                                     radioButtons("quote", "Quote",
+                                                  choices = c(None = "",
+                                                              "Double Quote" = '"',
+                                                              "Single Quote" = "'"),
+                                                  selected = '"')),
+                    hr(),
+                    uiOutput("itemColsChooser"),
+                    hr(),
+                    uiOutput("groupColChooser"),
+                    hr(),
+                    actionButton("goDescrStats",
+                                 "Calculate descriptive statistics!")
+                ),
+                mainPanel(
+                    h3("Raw data:"),
+                    dataTableOutput("dataOverview")
+                )
             )
-        ),
-        column(9,
-               h3("Overview of the data:"),
-               tableOutput("dataOverview"),
-               h3(textOutput("caption1", container = span)),
-               tableOutput("descrTable"))
+        )
     )
 )
