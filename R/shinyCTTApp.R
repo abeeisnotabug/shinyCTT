b8dcdb311da4de305ac4383a9657d03cd361ff74
R. Maximilian Bee <richard.maximilian.bee@uni-jena.de>
2026-04-20 10:43:43 +0200

    Clean up shinyCTTApp function.

--- R/shinyCTTApp.R
+++ R/shinyCTTApp.R
@@ -1,13 +1,5 @@
 #' @export
 shinyCTTApp <- function() {
-  # appDir <- system.file("shinyCTTApp", package = "shinyCTT")
-  # if (appDir == "") {
-  #   stop("Could not find directory. Try re-installing `shinyCTT`.", call. = FALSE)
-  # }
-
-  # For some reason, the app lags on startup if mvn has not run before at least once
-  # suppressMessages(invisible(MVN::mvn(rnorm(10))))
-
   shiny::shinyApp(
     ui = ui,
     server = server,
